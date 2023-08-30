4ec37f5dcb554c048cd4c926b1cdf84323a3b19f
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-08-30 14:06:37 -0300

    add button and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import './App.css'
+import { Button } from './components/Button'
 
 export function App(){
   return(
-    <h1>teste</h1>
+    <div className="Container">
+      <h1>Cálculo de IMC</h1>
+      <Button title='Calcular IMC'/>
+    </div>
   )
 }
